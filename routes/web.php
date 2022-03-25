4d481904e5500f78c595e6b83dbb55f0950d8624
AliceColombari <colombari.23@gmail.com>
2022-03-25 13:50:34 +0100

    esercizio ok!

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+                "stringa"=>'Hello World!'
+            ];
+            
+    return view('home', $data);
 });
